d21dba64e220db8a70fa3f4e5c488f70d9834abf
gabrieltoadere <gabriel.toadere.2024@mumail.ie>
2025-03-03 17:33:08 +0000

    Add files via upload

---
+++ AI-Guardian/textfile.java
@@ -0,0 +1,3 @@
+/*  this file contains instructions for any problems we encoutner along the way that we 
+    need to inform the rest about, anything thaty doesnt have a correlation to the task you are working on 
+    in the main project and anything that you feel wont make sense to talk about in your push comments*/
